6e08eac149d2411e5e3fa4577a0d424231b5e088
Jacob Tunnicliff <jtunnicliff@my.wctc.edu>
2025-11-11 13:36:32 -0600

    display catagories

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using NLog;
 using System.Linq;
 using NorthwindConsole.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
@@ -15,32 +17,28 @@ do
 
 
 {
-
-
   Console.WriteLine("1) Display categories");
-
-
   Console.WriteLine("2) Add category");
-
-
   Console.WriteLine("Enter to quit");
-
-
   string? choice = Console.ReadLine();
-
-
   Console.Clear();
-
-
   logger.Info("Option {choice} selected", choice);
-
-
-
-
-
   if (choice == "1")
-  {
+    {
     // display categories
+    var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+    var config = configuration.Build();
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
   }
   else if (choice == "2")
   {
